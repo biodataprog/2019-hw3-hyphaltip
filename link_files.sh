b0f0397cbba68ea899f7eae49c4f6766d07929d3
Jason Stajich <jasonstajich.phd@gmail.com>
2019-12-01 19:56:53 -0800

    download pfam2go

--- link_files.sh
+++ link_files.sh
@@ -3,3 +3,5 @@ mkdir -p fastq
 ln -s /bigdata/gen220/shared/data/M_tuberculosis/M_tuberculosis.cds.fasta .
 ln -s /bigdata/gen220/shared/data/M_tuberculosis/M_tuberculosis.pep.fasta .
 ln -s /bigdata/gen220/shared/data/M_tuberculosis/*.fastq.gz fastq/
+
+curl -O http://current.geneontology.org/ontology/external2go/pfam2go
